86e30d3e6da6f99520667d4e186112feecd67066
AndreaNaomi <kutaninaomi@gmail.com>
2022-09-12 18:20:23 -0300

    form pronto

--- src/pages/CreateProject/index.js
+++ src/pages/CreateProject/index.js
@@ -1,9 +1,86 @@
+import axios from "axios";
+import { useState } from "react";
 
 function CreateProject() {
-    return (
-        <div>
-        </div>
-    )
+  const [form, setForm] = useState({
+    nomeprojeto: "",
+    descprojeto: "",
+    data: "",
+    opcoesprioridades: "",
+    statusProjeto: "",
+  });
+
+  function handleChange(e) {
+    setForm({ ...form, [e.target.name]: e.target.value });
+  }
+
+  async function handleSubmit(e) {
+    e.preventDefault();
+    try {
+      await axios.post("https://ironrest.herokuapp.com/85-wd-project", form);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  console.log(form);
+
+  return (
+    <div>
+      <form
+        style={{ display: "flex", flexDirection: "column", width: "25%" }}
+        onSubmit={handleSubmit}
+      >
+        <label>Nome do Projeto</label>
+        <input
+          type="text"
+          name="nomeprojeto"
+          placeholder="Nome do Projeto"
+          onChange={handleChange}
+        />
+
+        <label>Descrição do Projeto</label>
+        <input
+          type="text"
+          name="descprojeto"
+          placeholder="Descrição do Projeto"
+          onChange={handleChange}
+        />
+
+        <label>Data de Conclusão</label>
+        <input
+          type="date"
+          name="data"
+          placeholder="Data de Conclusão"
+          onChange={handleChange}
+        />
+
+        <label>Prioridade</label>
+        <select
+          className="form-control"
+          name="opcoesprioridades"
+          onChange={handleChange}
+        >
+          <option>Baixa</option>
+          <option>Média</option>
+          <option>Alta</option>
+        </select>
+
+        <label>Status do Projeto</label>
+        <select
+          className="form-control"
+          name="statusProjeto"
+          onChange={handleChange}
+        >
+          <option>Em dia</option>
+          <option>Em risco</option>
+          <option>Em atraso</option>
+        </select>
+
+        <button type="submit">subbmit</button>
+      </form>
+    </div>
+  );
 }
 
 export default CreateProject;
--- src/pages/PáginaUsuario/index.js
+++ src/pages/PáginaUsuario/index.js
@@ -1,50 +1,60 @@
 import axios from "axios";
-import { useState, useEffect } from "react";
+import { useState } from "react";
 
 function PaginaUsuario() {
+  const [form, setForm] = useState({
+    nome: "",
+    email: "",
+    departamento: "",
+    tarefas: [],
+    projetos: [],
+  });
 
-    const [form, setForm] = useState({
-        nome: "",
-        email: "",
-        departamento: "",
-        tarefas: [],
-        projetos: [],
-    });
+  function handleChange(e) {
+    setForm({ ...form, [e.target.name]: e.target.value });
+  }
 
-    function handleChange(e) {
-        setForm({...form, [e.target.name]: e.target.value});
+  async function handleSubmit(e) {
+    e.preventDefault();
+    try {
+      await axios.post("https://ironrest.herokuapp.com/85-wd-user", form);
+    } catch (error) {
+      console.log(error);
     }
+  }
 
-    async function handleSubmit(e) { 
-        e.preventDefault();
-        try {
-        await axios.post("https://ironrest.herokuapp.com/85-wd-user", form);
-            
-        } catch (error) {
-            console.log(error);
-        }
-    }
+  console.log(form);
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <label>Nome</label>
+        <input
+          type="text"
+          name="nome"
+          placeholder="Nome"
+          onChange={handleChange}
+        />
 
+        <label>Email</label>
+        <input
+          type="email"
+          name="email"
+          placeholder="Email"
+          onChange={handleChange}
+        />
 
-console.log(form)
-    return (
-        <div>
-            
-            <form onSubmit={handleSubmit}>
-                <label>Nome</label>
-                <input type="text" name="nome" placeholder="Nome" onChange={handleChange}/>
+        <label>Departamento</label>
+        <input
+          type="text"
+          name="departamento"
+          placeholder="Departamento"
+          onChange={handleChange}
+        />
 
-                <label>Email</label>
-                <input type="email" name="email" placeholder="Email" onChange={handleChange}/>
-
-                <label>Departamento</label>
-                <input type="text" name="departamento" placeholder="Departamento" onChange={handleChange}/>
-
-                <button type="submit">Salvar</button>
-            </form>
-
-        </div>
-    )
+        <button type="submit">Salvar</button>
+      </form>
+    </div>
+  );
 }
 
-export default PaginaUsuario
+export default PaginaUsuario;
