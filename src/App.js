a4d749a2bf5b2bb8229af2eeac8d36b997bed95e
Rodrigo Dias <rodrigovicente.dev@gmail.com>
2022-09-14 15:26:33 -0300

    crud quase pronto, falta o delete

--- src/App.js
+++ src/App.js
@@ -5,11 +5,15 @@ import HomePage from './pages/HomePage';
 import PaginaUsuario from './pages/PáginaUsuario';
 import TelaTarefa from './pages/TelaTarefa';
 import CreateProject from './pages/CreateProject';
+import DetalhesProjetos from './pages/detalhesProjetos';
+import DetalheTarefa from './pages/detalhesTarefas';
+import UserDetalhe from './pages/userDetalhe';
+import NavBar from './components/NavBAr';
 
 function App() {
   return (
     <div className="App">
-
+      <NavBar/>
 
     <Routes>
 
@@ -18,7 +22,9 @@ function App() {
     <Route path="/PaginaUsuario" element={<PaginaUsuario />} />
     <Route path="/TelaTarefa" element={<TelaTarefa />} />
     <Route path="/CreateProject" element={<CreateProject />} />
-
+    <Route path="/projetos/:id" element={<DetalhesProjetos />}/>
+    <Route path='/tarefa/:id' element={<DetalheTarefa/>}/>
+    <Route path='/usuario/:id' element={<UserDetalhe/>}/>
     </Routes>
 
     </div>
