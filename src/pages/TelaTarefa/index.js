0d9f39ea9191d0846ed08b59c611b70d26c2935d
Rodrigo Dias <rodrigovicente.dev@gmail.com>
2022-09-16 13:30:31 -0300

    margen descoladas

--- src/pages/TelaTarefa/index.js
+++ src/pages/TelaTarefa/index.js
@@ -4,14 +4,13 @@ import UserPool from "../../components/EditarUsuario/userPoll";
 import Modal from "react-bootstrap/Modal";
 import { useNavigate } from "react-router-dom";
 
-
-function TelaTarefa({ projID, projetos, setProjetos, show, setShow }) {
-
-  const [carregar,setCarregar] = useState(true)
+function TelaTarefa({ projID, show, setShow }) {
+  const [carregar, setCarregar] = useState(true);
   const [usuarios, setUsuarios] = useState([]);
   const [tarefas, setTarefas] = useState([]);
   const [loading, setLoading] = useState("false");
   const [listaUser, setListaUser] = useState([]);
+  const [projetos, setProjetos] = useState([]);
   const [form, setForm] = useState({
     id_t: 300,
     tarefa: "",
@@ -21,7 +20,7 @@ function TelaTarefa({ projID, projetos, setProjetos, show, setShow }) {
     usuario: [],
   });
   const navigate = useNavigate();
-
+let projIDt =  projID +1
   function handleChange(e) {
     setForm({ ...form, [e.target.name]: e.target.value });
   }
@@ -51,6 +50,18 @@ function TelaTarefa({ projID, projetos, setProjetos, show, setShow }) {
     setLoading(false);
     async function fecthUsuario() {
       let response = await axios.get(
+        "https://ironrest.herokuapp.com/85-wd-project"
+      );
+      setProjetos(response.data);
+      setLoading(true);
+    }
+    fecthUsuario();
+  }, []);
+
+  useEffect(() => {
+    setLoading(false);
+    async function fecthUsuario() {
+      let response = await axios.get(
         "https://ironrest.herokuapp.com/85-wd-user"
       );
       setUsuarios(response.data);
@@ -60,8 +71,10 @@ function TelaTarefa({ projID, projetos, setProjetos, show, setShow }) {
   }, []);
 
   function vincularProjeto() {
+    console.log('chamou')
     projetos.forEach((element) => {
-      if (element.id_p == projID) {
+      console.log('chamou2')
+      if (element.id_p == projIDt) {
         const clonep = { ...element };
         clonep.tarefas.push(tarID + 1);
 
@@ -70,13 +83,15 @@ function TelaTarefa({ projID, projetos, setProjetos, show, setShow }) {
     });
   }
 
-  async function Submitp(projeto) {
+  async function Submitp(projetoss) {
+   console.log(projetoss)
+
     try {
-      let id = projeto._id;
-      delete projeto._id;
+      let id = projetoss._id;
+      delete projetoss._id;
       await axios.put(
         `https://ironrest.herokuapp.com/85-wd-project/${id}`,
-        projeto
+        projetoss
       );
     } catch (error) {
       console.log(error);
@@ -106,10 +121,9 @@ function TelaTarefa({ projID, projetos, setProjetos, show, setShow }) {
   async function handleSubmit(e) {
     e.preventDefault();
 
-    console.log(tarID);
     appendar();
 
-    vincularProjeto();
+    vincularProjeto()
 
     try {
       await axios.post("https://ironrest.herokuapp.com/85-wd-to-do", form);
@@ -122,83 +136,88 @@ function TelaTarefa({ projID, projetos, setProjetos, show, setShow }) {
   function adicionarUsers() {
     listaUser.forEach((element) => {
       form.usuario.push(element.id);
-      setCarregar(false)
+      setCarregar(false);
     });
   }
   return (
-    <div style={{padding:'20px'}}>  
-   <Modal show={show} onHide={() => setShow(false)}>
-      <Modal.Header closeButton>
-        <Modal.Title>Adicionar Tarefa</Modal.Title>
-      </Modal.Header>
-      <Modal.Body>
-     
-      <div>
-        <form onSubmit={handleSubmit}>
+    <div style={{ padding: "20px" }}>
+      <Modal show={show} onHide={() => setShow(false)}>
+        <Modal.Header closeButton>
+          <Modal.Title>Adicionar Tarefa</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <div>
+            <form onSubmit={handleSubmit}>
+              <div class="mb-3">
+                <label label for="formGroupExampleInput" class="form-label">
+                  Nome da Tarefa
+                </label>
+                <input
+                  class="form-control"
+                  id="formGroupExampleInput"
+                  type="text"
+                  name="tarefa"
+                  placeholder="Nome da tarefa"
+                  onChange={handleChange}
+                />
+              </div>
+              <div class="mb-3">
+                <label label for="formGroupExampleInput" class="form-label">
+                  Descrição da Tarefa
+                </label>
+                <input
+                  class="form-control"
+                  id="formGroupExampleInput"
+                  type="text"
+                  name="descrição"
+                  placeholder="Descrição da Tarefa"
+                  onChange={handleChange}
+                />
+              </div>
+              <div class="mb-3">
+                <label label for="formGroupExampleInput" class="form-label">
+                  Dead Line
+                </label>
+                <input
+                  class="form-control"
+                  id="formGroupExampleInput"
+                  type="date"
+                  name="data"
+                  placeholder="Nome da tarefa"
+                  onChange={handleChange}
+                />
+              </div>
+              <button className="btn btn-primary" type="submit">
+                Salvar
+              </button>
+            </form>
+          </div>
 
-          <div class="mb-3">
-            <label label for="formGroupExampleInput" class="form-label">
-              Nome da Tarefa
-            </label>
-            <input class="form-control" id="formGroupExampleInput"
-              type="text"
-              name="tarefa"
-              placeholder="Nome da tarefa"
-              onChange={handleChange}
-            />
-          </div>
-          <div class="mb-3">
-            <label label for="formGroupExampleInput" class="form-label">
-              Descrição da Tarefa
-            </label>
-            <input class="form-control" id="formGroupExampleInput"
-              type="text"
-              name="descrição"
-              placeholder="Descrição da Tarefa"
-              onChange={handleChange}
-            />
-          </div>
-          <div class="mb-3">
-            <label label for="formGroupExampleInput" class="form-label">
-              Dead Line
-            </label>
-            <input class="form-control" id="formGroupExampleInput"
-              type="date"
-              name="data"
-              placeholder="Nome da tarefa"
-              onChange={handleChange}
-            />
-          </div>
-          <button className="btn btn-primary" type="submit">
-            Salvar
-          </button>
+          <>
+            <br></br>
+            <UserPool listaUser={listaUser} setListauser={setListaUser} />
+          </>
+          {carregar && (
+            <div>
+              {listaUser.map((element) => {
+                return (
+                  <p style={{ borderBottom: "1px black solid" }}>
+                    {element.nome}
+                  </p>
+                );
+              })}
+              <button
+                className="btn btn-secondary btn-sm"
+                onClick={adicionarUsers}
+              >
+                Adicionar
+              </button>
+            </div>
+          )}
+        </Modal.Body>
 
-        </form>
-      </div>
-      
-        <>
-      <br></br>
-      <UserPool listaUser={listaUser} setListauser={setListaUser} />
-      </>
-      {carregar&&(
-      <div>
-        {listaUser.map((element) => {
-          return <p style={{borderBottom: '1px black solid'}}>{element.nome}</p>;
-        })}
-        <button className="btn btn-secondary btn-sm" onClick={adicionarUsers}>Adicionar</button>
-      </div>
-        )}
-      </Modal.Body>
-      
-      <Modal.Footer className="justify-content-between">
-      </Modal.Footer>
+        <Modal.Footer className="justify-content-between"></Modal.Footer>
       </Modal>
-
-
-
-
-
-      
     </div>
   );
 }
